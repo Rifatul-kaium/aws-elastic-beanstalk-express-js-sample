30d39452eebe292e4a26c61ed74842d1fb635c82
Rifatul Kaium <69492120+Rifatul-kaium@users.noreply.github.com>
2024-07-07 14:11:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Rifa!'));
+app.get('/', (req, res) => res.send('Hello Rifa, Welcome to the IT World !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
